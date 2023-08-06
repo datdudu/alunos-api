76c2b50e7f13e3f89d172ff50efa7685676e37fb
Eduardo Carvalho Cardoso <dxcar10000@hotmail.com>
2023-08-06 18:48:17 -0300

    Adding Modal to Include Student

--- studentsreact/src/App.js
+++ studentsreact/src/App.js
@@ -15,6 +15,29 @@ function App() {
   const baseUrl="https://localhost:44357/api/students";
 
   const [data, setData]= useState([]);
+  const [modalInclude, setModalInclude] = useState(false);
+
+  const [selectedStudent, setSelectedStudent]=useState({
+    id: '',
+    name: '',
+    email: '',
+    age: ''
+  })
+
+  const handleChange =e =>{
+    const {name, value}=e.target;
+
+    setSelectedStudent({
+      ...selectedStudent,
+      [name]: value
+    });
+
+    console.log(selectedStudent);
+  }
+  
+  const openCloseModalInclude=()=>{
+    setModalInclude(!modalInclude);
+  }
 
   const requestGet = async () => {
     try {
@@ -25,19 +48,32 @@ function App() {
     }
   }
 
+  const requestPost = async () => {
+    delete selectedStudent.id;
+
+    selectedStudent.age = parseInt(selectedStudent.age);
+      await axios.post(baseUrl, selectedStudent)
+    .then(response=>{
+      setData(data.concat(response.data));
+      openCloseModalInclude();
+    }).catch(error=>{
+      console.log(error);
+    })
+  }
+
   useEffect(()=>{
     requestGet();
   })
 
 
   return (
-    <div className="App">
+    <div className="students-container">
       <br />
       <h3>Students Register</h3>
 
-      <header className="App-header">
+      <header>
         <img src={logoRegister} alt="logo" />
-        <button type="button" className="btn btn-succes">Add Student</button>
+        <button type="button" className="btn btn-succes" onClick={()=>openCloseModalInclude()}>Add Student</button>
       </header>
 
       <table className="table table-bordered">
@@ -65,6 +101,32 @@ function App() {
           ))}
         </tbody>
       </table>
+
+      <Modal isOpen={modalInclude}>
+        <ModalHeader>Add Student</ModalHeader>
+
+        <ModalBody>
+          <div className="form-group">
+            <label>Name: </label>
+            <br/>
+            <input type="text" className="form-control" name='name' onChange={handleChange} />
+            <br/>
+            <label>Email: </label>
+            <br/>
+            <input type="text" className="form-control" name='email' onChange={handleChange} />
+            <br/>
+            <label>Age: </label>
+            <br/>
+            <input type="text" className="form-control" name='age' onChange={handleChange} />
+            <br/>
+          </div>
+        </ModalBody>
+
+        <ModalFooter>
+          <button type="button" className="btn btn-primary" onClick={()=>requestPost()}>Add</button>
+          <button type="button" className="btn btn-danger"  onClick={()=>openCloseModalInclude()}>Cancel</button>
+        </ModalFooter>
+      </Modal>
     </div>
   );
 }
